6fe1e8a3b44e466a901e70ddbb02fa802911c28a
joemiltonm <m.joemilton@gmail.com>
2023-10-17 10:35:08 +0530

    till db schema creation 58:44

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,8 @@
+/* If your [...nextauth].ts file is directly inside the 
+pages/api folder without an auth folder, then the URL 
+pattern would indeed be /api/*.
+
+In this specific configuration, the [...nextauth].ts file 
+would catch all API requests that match the pattern /api/* and handle 
+them through NextAuth.js.*/
+
